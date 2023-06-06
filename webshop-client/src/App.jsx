d3a8adc32f3190021a676d55f3c6d523ab97895a
rotte2000217 <rotte2000217@protonmail.com>
2023-06-06 15:58:32 +0200

    Add header with navbar

--- webshop-client/src/App.jsx
+++ webshop-client/src/App.jsx
@@ -1,7 +1,20 @@
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
+import Container from "react-bootstrap/Container";
+import Header from "./components/Header/Header";
+import Home from "./pages/Home/Home";
 
 function App() {
-  return <div className="App">Webshop App</div>;
+  return (
+    <>
+      <Header />
+      <Container>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+        </Routes>
+      </Container>
+    </>
+  );
 }
 
 export default App;
---
+++ webshop-client/src/components/Header/Header.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <Navbar bg="dark" variant="dark" expand="md">
+      <Container>
+        <Navbar.Brand as={Link} to="/">
+          Webshop
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link as={Link} to="/">
+              Home
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default Header;
